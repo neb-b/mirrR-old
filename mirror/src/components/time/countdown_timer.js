265aa9a7070c7f0395a85c7173768a11729e05d9
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-07-25 16:58:33 -0700

    Fix view for timers less then 10 seconds

--- mirror/src/components/time/countdown_timer.js
+++ mirror/src/components/time/countdown_timer.js
@@ -43,6 +43,7 @@ export default class CountdownTimer extends Component {
     const minutes = Math.floor(time / 60)
     let seconds = time - minutes * 60
     if (!seconds) seconds = '00'
+    if (seconds < 10) seconds = '0' + seconds
 
     return `${minutes}:${seconds}`
   }
