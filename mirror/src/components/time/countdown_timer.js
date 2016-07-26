e7fe5fe217a8eadb0edee5da73f8f08f574f4a20
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-07-26 13:24:53 -0700

    Speech to start timer partially working

--- mirror/src/components/time/countdown_timer.js
+++ mirror/src/components/time/countdown_timer.js
@@ -18,48 +18,57 @@ export default class CountdownTimer extends Component {
     }
   }
 
-  componentDidMount() {
-    if (annyang) {
-      var commands = {
-        'start timer': this.newTimer(2),
-        'start a timer': this.newTimer(2),
-        'new timer': this.newTimer(2),
-        'timer': this.newTimer(2),
-        'start timer for *time minutes': this.newTimer(minutes),
-        'start timer for *minutes minutes and *seconds': this.newTimer(minutes, seconds),
-        'stop timer': this.stopTimer()
-      }
-
-      annyang.addCommands(commands)
-      annyang.start({continuous: false})
-    }
-  }
-
-  newTimer(minutes, seconds) {
-    let time = minutes * 60 + seconds
-    this.setState({
-      currentTimerValue: time,
-      initialTimerValue: time
-    })
-  }
-
-  stopTimer() {
-    this.setState({
-      currentTimerValue: null,
-      initialTimerValue: null
-    })
-  }
-
-  // If current timer value, format seconds into mm:ss
-  // If no curren timer, return null
-  getFormattedTime(time) {
-    const minutes = Math.floor(time / 60)
-    let seconds = time - minutes * 60
-    if (seconds < 10) seconds = '0' + seconds
-    if (!seconds) seconds = '00'
-
-    return `${minutes}:${seconds}`
-  }
+  // Needs more testing
+  //
+  // componentDidMount() {
+  //   if (annyang) {
+  //     var commands = {
+  //       'start timer': this.newTimer(2),
+  //       'start a timer': this.newTimer(2),
+  //       'new timer': this.newTimer(2),
+  //       'timer': this.newTimer(2),
+  //       'start timer for *length minutes': this.newTimer(length),
+  //       // 'start timer for *length minutes and *seconds': this.newTimer(length, seconds),
+  //       'stop timer': this.stopTimer()
+  //     }
+  //
+  //     annyang.addCommands(commands)
+  //     annyang.start({continuous: false})
+  //   }
+  // }
+  //
+  // newTimer(minutes, seconds) {
+  //   console.log('New timer: ', minutes, seconds)
+  //   let time
+  //
+  //   if (!seconds) {
+  //     time = minutes * 60
+  //   } else {
+  //     time = minutes * 60 + seconds
+  //   }
+  //   this.setState({
+  //     currentTimerValue: time,
+  //     initialTimerValue: time
+  //   })
+  // }
+  //
+  // stopTimer() {
+  //   this.setState({
+  //     currentTimerValue: null,
+  //     initialTimerValue: null
+  //   })
+  // }
+  //
+  // // If current timer value, format seconds into mm:ss
+  // // If no curren timer, return null
+  // getFormattedTime(time) {
+  //   const minutes = Math.floor(time / 60)
+  //   let seconds = time - minutes * 60
+  //   if (seconds < 10) seconds = '0' + seconds
+  //   if (!seconds) seconds = '00'
+  //
+  //   return `${minutes}:${seconds}`
+  // }
 
   render() {
     if (!this.state.currentTimerValue) return null
