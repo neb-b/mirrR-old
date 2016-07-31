05ec7227bc08ada117897216c79031eecbbc9a36
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-07-31 01:54:58 -0700

    console log

--- mirror/src/actions/action_weather.js
+++ mirror/src/actions/action_weather.js
@@ -3,6 +3,7 @@ import axios from 'axios'
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 
 export function fetchWeather(pos) {
+  console.log('pos', pos)
   // const url = `https://react-mirror-server.herokuapp.com/weather`
   const url = `http://localhost:5000/weather`
 
