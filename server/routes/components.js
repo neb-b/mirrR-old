2f785ebb2d45a42e03b73495e741f17c7f127850
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-07-31 21:42:02 -0700

    Testing

--- server/routes/components.js
+++ server/routes/components.js
@@ -6,13 +6,14 @@ const router = express.Router();
 let components = []
 
 router.get('/', function(req, res, next) {
-  console.log('components', components)
+  console.log('sending components', components)
   res.send({ components: components })
 })
 
 router.put('/', function(req, res) {
   const io = req.io
   components = req.body.components || ""
+  console.log('new components', components)
   io.emit('update-components', { components: components })
   res.send({ components: components })
 })
