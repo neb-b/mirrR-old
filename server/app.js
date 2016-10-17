eacf11a182ef8aef4f004f5d2587ca3e1fe2874b
Sean Yesmunt <sean.yesmunt@gmail.com>
2016-10-16 21:51:35 -0700

    Add console log on server start

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 server.listen(process.env.PORT || 5000)
+console.log('server running on port 5000')
 
 // Add headers
 app.use(function (req, res, next) {
